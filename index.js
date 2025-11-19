704215ba081a6669d059fc2db3078205c258b286
ankhemanta <ankhemanta@gmail.com>
2025-11-19 20:58:20 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+import { SafeAreaView } from "react-native-safe-area-context";
+import {
+  scale,
+  verticalScale,
+  moderateScale
+} from 'react-native-size-matters';
+
+
+
+
+
+export {
+  SafeAreaView,
+  scale,
+  verticalScale,
+  moderateScale
+}
+
+
+
+
